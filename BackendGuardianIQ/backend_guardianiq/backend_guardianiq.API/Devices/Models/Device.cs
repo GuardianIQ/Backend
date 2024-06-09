066d9eaadbf7fafc173554240169257a7af7cd27
Seba <sebalobato05@gmail.com>
2024-06-08 21:16:38 -0500

    Create Device.cs

---
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/Devices/Models/Device.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_guardianiq.API.Devices.Models;
+
+public class Device
+{
+    public int Id { get; set; }
+    public string Tipo { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public string Ubicacion { get; set; }
+    public string Imagen { get; set; }
+}
