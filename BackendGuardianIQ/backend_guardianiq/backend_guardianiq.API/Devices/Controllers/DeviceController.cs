6b0c2943a7b93689a8c4babaf11872a35ad03969
Seba <sebalobato05@gmail.com>
2024-06-08 21:16:24 -0500

    Create DeviceController.cs

---
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/Devices/Controllers/DeviceController.cs
@@ -0,0 +1,70 @@
+﻿using backend_guardianiq.API.Devices.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace backend_guardianiq.API.Devices.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DeviceController : ControllerBase
+{
+    private static List<Device> Devices = new List<Device>();
+
+    [HttpGet]
+    public IEnumerable<Device> GetDevices()
+    {
+        return Devices;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Device> GetDevice(int id)
+    {
+        var device = Devices.FirstOrDefault(d => d.Id == id);
+        if (device == null)
+        {
+            return NotFound();
+        }
+        return device;
+    }
+
+    [HttpPost]
+    public ActionResult<Device> CreateDevice(Device device)
+    {
+        Devices.Add(device);
+        return CreatedAtAction(nameof(GetDevice), new { id = device.Id }, device);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateDevice(int id, Device device)
+    {
+        if (id != device.Id)
+        {
+            return BadRequest();
+        }
+
+        var existingDevice = Devices.FirstOrDefault(d => d.Id == id);
+        if (existingDevice == null)
+        {
+            return NotFound();
+        }
+
+        existingDevice.Tipo = device.Tipo;
+        existingDevice.Marca = device.Marca;
+        existingDevice.Modelo = device.Modelo;
+        existingDevice.Ubicacion = device.Ubicacion;
+        existingDevice.Imagen = device.Imagen;
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteDevice(int id)
+    {
+        var device = Devices.FirstOrDefault(d => d.Id == id);
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        Devices.Remove(device);
+        return NoContent();
+    }
+}
