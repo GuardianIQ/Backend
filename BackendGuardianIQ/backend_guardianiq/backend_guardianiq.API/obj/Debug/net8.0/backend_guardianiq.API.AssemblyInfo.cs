29ea2d562dedd28225feeb36ff6d2e37c11c396d
Seba <sebalobato05@gmail.com>
2024-06-08 21:16:16 -0500

    Created devices

--- BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1647967534a82cc7d2908b5dc598a2b9fe1594")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
