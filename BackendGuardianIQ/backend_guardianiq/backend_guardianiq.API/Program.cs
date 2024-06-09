e679676f4d8063d9d1a458c0deaae446637d1063
Seba <sebalobato05@gmail.com>
2024-06-08 22:31:15 -0500

    Update Program.cs

--- BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/Program.cs
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/Program.cs
@@ -1,11 +1,44 @@
+using backend_guardianiq.API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers(); // Add this line
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();    
+    });
+
+
 var app = builder.Build();
+
+// Verify Database Objects area Created
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,7 +49,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapControllers(); // Add this line
+app.MapControllers();
 
 app.Run();
-
