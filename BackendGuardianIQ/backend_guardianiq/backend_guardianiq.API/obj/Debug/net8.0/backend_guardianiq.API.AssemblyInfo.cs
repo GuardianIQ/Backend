47575e2848ae8cbad14b4aec93f8831ee04ca67a
Seba <sebalobato05@gmail.com>
2024-06-08 22:32:54 -0500

    Dependencies updates

--- BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/obj/Debug/net8.0/backend_guardianiq.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1647967534a82cc7d2908b5dc598a2b9fe1594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066d9eaadbf7fafc173554240169257a7af7cd27")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_guardianiq.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
