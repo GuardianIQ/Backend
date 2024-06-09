f0a7da2e897a6362486efde58de944075c76c1d7
Seba <sebalobato05@gmail.com>
2024-06-08 22:30:22 -0500

    Create appDbContext.cs

---
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/appDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend_guardianiq.API.Devices.Models;
+
+namespace backend_guardianiq.API
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Device> Devices { get; set; }
+    }
+}
