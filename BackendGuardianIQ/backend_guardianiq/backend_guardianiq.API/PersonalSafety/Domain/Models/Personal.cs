7bedc3f707fdce396f884e98b966b8f7e492cf71
rivacortezf <u202215313@upc.edu.pe>
2024-06-11 03:13:55 -0500

    feat:add model entity (PersonalSafety)

---
+++ BackendGuardianIQ/backend_guardianiq/backend_guardianiq.API/PersonalSafety/Domain/Models/Personal.cs
@@ -0,0 +1,15 @@
+namespace backend_guardianiq.API.PersonalSafety.Domain.Models;
+
+public class Personal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string District { get; set; }
+    public int Age { get; set; }
+    public string Sex { get; set; }
+    public int Experience { get; set; }
+    public string Avality { get; set; }
+    public string Contract { get; set; }
+    public string Photo { get; set; }
+    
+}
